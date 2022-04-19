ccac5a5cd3e1135e971ad7963fda8739350a2b40
LuizFelipe883 <luizfelipedesouza94@gmail.com>
2022-04-19 19:44:08 -0300

    aula 04 e 05

---
+++ 04-events/index.js
@@ -0,0 +1,32 @@
+const EventEmiter = require('events')
+const { mainModule } = require('process')
+class MeuEmissor extends EventEmiter {
+
+
+}
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuarioClick'
+meuEmissor.on(nomeEvento, function(click) {
+    console.log('um usuario clicou', click)
+})
+
+/* meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+meuEmissor.emit(nomeEvento, 'no ok')
+
+let count = 0
+setInterval(function (){
+    meuEmissor.emit(nomeEvento, 'no ok' + (count++))
+}, 1000) */
+
+const stdin = process.openStdin()
+function main() {
+    return new Promise(function (resolve, reject) {
+        stdin.addListener('data',function(value) {
+            //console.log(`Voce digitou: ${value.toString().trim()}`)
+            return resolve(value)
+        })
+    })
+}
+main().then(function(resultado) {
+    console.log('resultado', resultado.toString())
+})
